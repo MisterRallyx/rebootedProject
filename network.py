200535b34f8bf0fde7dde8cf303213674ac4d4bd
Alexis Bandet <alexis.bandet@etu.u-bordeaux.fr>
2018-04-17 15:17:59 +0200

    Recuperation du modele par le client

--- network.py
+++ network.py
@@ -25,11 +25,58 @@ class NetworkServerController:
         #list of socket
         self.sockList = [self.sock]
 
-    # send model to all client
-    def sendModel(self, model):
-        for sock in sockList:
-            if sock != self.sock:
-                self.sock.sendto(model, sock)
+    # send model to client sockets
+    def sendModel(self, socket, sendMap = False):
+        model_str = "MODEL "
+        # if client ask for map
+        if sendMap == True :
+            model_str += str(self.model.map)
+            #sendMap()
+        # string of all characters
+        for character in self.model.characters :
+            model_str = self.add_character_to_str(model_str, character)
+        # string of all bombs
+        for bomb in self.model.bombs :
+            model_str = self.add_bomb_to_str(model_str, character)
+        # string of all fruits
+        for fruit in self.model.fruits :
+            model_str = self.add_fruit_to_str(model_str, fruit)
+        model_str += "END"
+        socket.send(model_str.encode())
+
+    # transform a character object into a string
+    def add_character_to_str(self, str, character):
+        str_m += "CHARACTER "
+        str_m += str(character.kind) + " "
+        str_m += str(character.health) + " "
+        str_m += str(character.immunity) + " "
+        str_m += str(character.disarmed) + " "
+        str_m += str(character.nickname) + " "
+        str_m += str(character.pos[0]) + " " + str(character.pos[1]) + " "
+        str_m += str(character.direction) + " "
+        return str_m
+
+    # transform a bomb object into string
+    def add_bomb_to_str(self, str_m, bomb):
+        str_m+= "BOMB "
+        str_m += str(bomb.pos) + " "
+        str_m += str(bomb.max_range) + " "
+        str_m += str(bomb.countdown) + " "
+        str_m += str(bomb.time_to_explode) + " "
+        return str_m
+
+    # transform a fruit object into string
+    def add_fruit_to_str(self, str_m, fruit):
+        str_m += "FRUIT "
+        str_m += str(fruit.pos[0]) + " " + str(fruit.pos[1]) + " "
+        str_m += str(fruit.kind) + " "
+        return str_m
+
+    # treat a received message
+    def treat(self, message, socket):
+        if message==b"GET":
+            self.sendModel(socket)
+
     # time event
     def tick(self, dt):
         read_list, _, _ = select.select(self.sockList, [], [])
@@ -38,13 +85,14 @@ class NetworkServerController:
             if sock == self.sock:
                 sclient, addr = self.sock.accept()
                 self.sockList.append(sclient)
-                print('Connected by', addr)
+                print('Connection by', addr)
+                # TODO: add character
             # socket client (new client message)
             else:
                 while True:
                     # TODO: treat data
                     data = sock.recv(1500)
-                    print(data)
+                    self.treat(data, sock)
                     if data == b'' or data == b'\n' :
                         print('Disconnected by')
                         self.sockList.remove(sock)
@@ -66,10 +114,58 @@ class NetworkClientController:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # connexion to server
         try :
-            self.sock.connect((host, port))
+            self.sock.connect((self.host, self.port))
         except :
-            print('Unable to connect')
+            print('Unable to connect to {}. Please try later.'.format(host))
             sys.exit()
+        self.getModel()
+
+    # get initial Model
+    def getModel(self):
+        self.sock.send(b"GET")
+        data = self.sock.recv(1500)
+        self.treatData(data)
+
+    # treat received data for updating the client model
+    def treatData(self, data):
+        data = data.decode()
+        data_a = data.split(" ")
+        i = 0
+        # get the original model
+        if data_a[i]=="MODEL":
+            while data_a[i]!="END":
+                # add a fruit
+                if data_a[i]=="FRUIT":
+                    self.model.add_fruit(int(data_a[i+3]), (int(data_a[i+1]), int(data_a[i+2])))
+                    i+=3
+                # add a character
+                elif data_a[i]=="CHARACTER":
+                    kind_c = int(data_a[i+1])
+                    health_c = int(data_a[i+2])
+                    immunity_c = float(data_a[i+3])
+                    disarmed_c = float(data_a[i+4])
+                    nickname_c = data_a[i+5]
+                    position = (int(data_a[i+6]), int(data_a[i+7]))
+                    direction_c = data_a[i+8]
+                    self.model.add_character(nickname_c, isplayer=True, kind=kind_c, pos=position)
+                    for character in self.model.characters :
+                        if character.nickname == nickname_c :
+                            character.immunity = immunity_c
+                            character.disarmed = disarmed_c
+                            character.health = health_c
+                    i+=8
+                # add a bomb
+                elif data_a[i]=="BOMB":
+                    position = (int(data_a[i+1], int(data_a[i+2])))
+                    max_range_b = int(data_a[i+3])
+                    countdown_b = int(data_a[i+4])
+                    time_to_explode_b = float(data_a[i+5])
+                    self.model.bombs.append(Bomb(self.map, position))
+                    self.model.bombs[-1].countdown = countdown_b
+                    self.model.bombs[-1].time_to_explode = time_to_explode_b
+                    self.model.bombs[-1].max_range = max_range_b
+                    i+=5
+                i+=1
 
     # keyboard events
     def keyboard_quit(self):
@@ -88,5 +184,5 @@ class NetworkClientController:
 
     # time event
     def tick(self, dt):
-        # ...
+        #self.getModel()
         return True
